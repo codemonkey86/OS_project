63d134a0ec5ed77e3517d17085255f84ca3931ee
steve <steve@master.(none)>
2011-11-05 23:50:38 -0400

    initial functional check in to pi for calculating pi to 15 and load balancing

---
+++ pi/app/controllers/pi_controller.rb
@@ -0,0 +1,38 @@
+include Math
+class PiController < ApplicationController
+
+   def compute
+        if  !params[:digits].match(/^[1-9][0-9]*$/) or params[:digits].to_i > 15 
+             @result = "Invalid Input: Try Again"
+        else # use Gauss-Legendre algo to calculate pi
+            # initial values
+           
+           a = 1
+           b  = 1/sqrt(2)
+           t = 0.25
+           p = 1
+           found = false
+  	   while !found
+                 puts "iter" 
+                  an = (a+b)/2
+                  bn = sqrt(a*b)
+                  tn = t -p*(a-an)**2
+                  pn = 2*p
+                  a = an
+ 		  b = bn
+  		  t = tn
+                  p = pn
+                  pi = (a+b)**2/(4*t)
+                  if (a-b < 10**(-1*params[:digits].to_i) )
+                       found = true
+                  end
+           end
+
+	   @result = pi.to_s[0, params[:digits].to_i+2]
+        end
+             
+   end
+
+
+
+end
---
+++ pi/test/unit/helpers/pi_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PiHelperTest < ActionView::TestCase
+end
