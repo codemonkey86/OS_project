b0a0078ddc38b5710d6a9d8aa9c04ae7b1eeb75f
steve <steve@master.(none)>
2011-11-08 21:32:50 -0500

    updated services environment for new gems and updated database.yml files to use jruby for later utilization of peach

--- services/config/environment.rb
+++ services/config/environment.rb
@@ -20,8 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "curb"
-  config.gem "sqlite3"
-
+  config.gem "jdbc-sqlite3"
+  config.gem "activerecord-jdbcsqlite3-adapter"
+#   config.gem "nmap-parser"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
