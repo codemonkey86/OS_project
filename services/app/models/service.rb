436d12c36511884d140b1a1a2fe367711daea1cb
Phil Eberhardt <peberhardt@gmail.com>
2011-11-10 17:37:56 -0500

    slight update to tweak cache building using peach

--- services/app/models/service.rb
+++ services/app/models/service.rb
@@ -6,7 +6,7 @@ class Service < ActiveRecord::Base
     'convert' => '1210',
     'quad' => '4416'
   }
-  
+
   ##
   # returns a populated services object, generally for cacheing
   def self.discovery
@@ -14,46 +14,46 @@ class Service < ActiveRecord::Base
       :services => Hash.new{ |hash,key| hash[key] = Array.new },
       :timestamp => nil
     }
-   
-   # remote call to services/list
 
-   nmap.peach do |box|  
-      json_out = net_get("http://#{box}:3000/services/list")      
-   end 
-   
-     cache_me[:services][name] << json_out?  #this needs fixing 
-  
-    
+    # remote call to services/list
+    nmap.peach do |box|
+      json_out = net_get("http://#{box}:3000/services/list")
+      if json_out
+        JSON.parse(json_out).peach do |name|
+          cache_me[:services][name] << box
+        end
+      end
+    end
+
     cache_me[:timestamp] = DateTime.now
     cache_me
   end
-  
+
   def self.up?(name,host='localhost')
-   puts "http://#{host}:#{Service::APPS[name]}"
-    if !net_get("http://#{host}:#{Service::APPS[name]}")  
-     return false
-   else 
-    return true
+    logger.info "http://#{host}:#{Service::APPS[name]}"
+    if !net_get("http://#{host}:#{Service::APPS[name]}")
+      return false
+    else
+      return true
+    end
   end
-end
-  
+
   private
-  # Curb wrapper to catch any errors for connections
+  # wrapper to catch any errors for connections
   # input: url to connect to
   # output: false on error or curb response otherwise
   # TODO: would need to give back obj eventually- return a tuple status (curb respose or false,  object (or nil in case of false))
   def self.net_get(url)
-    
     begin
       res = Net::HTTP.get(URI.parse(url))
-    rescue Exception => e 
+    rescue Exception => e
       false
     end
   end
 
   #dummy cheat
   def self.nmap
-    s = "localhost stevebox"
-    [s.split(' ')[0]]
+    %w(localhost stevebox)
   end
 end
+
