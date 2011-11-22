d22c1d5c9a77d240916218714388dc7b1bdacb88
Phil Eberhardt <peberhardt@gmail.com>
2011-11-21 20:38:11 -0500

    some code cleanup and readability improvements

--- services/app/controllers/services_controller.rb
+++ services/app/controllers/services_controller.rb
@@ -1,19 +1,20 @@
 require 'peach'
 require 'json'
 class ServicesController < ApplicationController
-  # GET /services
-  # can pass param to negate building a new cached object
-  skip_before_filter :verify_authenticity_token 
   # this is disabled for the purpose of gathering metrics via exteranl ruby script
-  def index
+  skip_before_filter :verify_authenticity_token
 
-    render :json => Service.getindex 
+  # GET /services
+  def index
+    render :json => Service.getindex
   end
 
   # GET /services/noservice
   def noservice
   end
 
+  # GET /services/set_cache
+  # jump straight to cache writing via passed param
   def set_cache
     Rails.cache.write(Service.cache_key,JSON.parse(params[:newest]))
   end
@@ -25,28 +26,27 @@ class ServicesController < ApplicationController
       redirect_to root_path and return
     end
 
-   #load balancing algorithm: run locally if below threshold, else redirect request to lowest absolute load of discovered services
-    # fetch cache
+    # load balancing algorithm: run locally if below threshold
+    # else redirect request to lowest absolute load of discovered services
 
+    # check if cached, else build service knowledge from Service.discovery
     syscache = Rails.cache.fetch(Service.cache_key, :timeout => 1.hour) {Service.discovery}
+    service_info = syscache[:services][params[:id]]
     #run_local (service_name, service_policy on "localhost",  machine_policy, service_load_avg)
     if Service::APPS[params[:id]].nil?
-        render :action => "noservice"
-    elsif Service.run_local(params[:id], syscache[:services][params[:id]][:host_policy][`hostname`.strip], syscache[:machinepolicy], syscache[:services][params[:id]][:threshold])
-       puts "runlocal true"
+      render :action => "noservice"
+    elsif Service.run_local(params[:id],service_info[:host_policy][`hostname`.strip], syscache[:machinepolicy], service_info[:threshold])
+      puts "runlocal true"
       redirect_to "http://localhost:#{Service::APPS[params[:id]].first}/#{params[:id]}"
     else
-        minload = Service.minload(syscache[:services][params[:id]], syscache[:machinepolicy], Service::APPS[params[:id]].first)
-        if !minload
-            render :action =>  "noservice"
-        else
-             redirect_to minload 
-        end
-
+      minload = Service.minload(service_info, syscache[:machinepolicy], Service::APPS[params[:id]].first)
+      if !minload
+        render :action =>  "noservice"
+      else
+        redirect_to minload
+      end
     end
-
-end
-
+  end
 
   # GET /services/list
   def list
--- services/app/models/service.rb
+++ services/app/models/service.rb
@@ -21,67 +21,57 @@ class Service < ActiveRecord::Base
     load &&  (load.to_f < threshold)  && policymatch(servicepolicy,machinepolicy)
   end
 
-  def self.minload(scache, policyreq, port)  # find machine that is up with absolute minimum load and appropriate policy 
-                                     #(make sure it is up before assigning it as "low", tkaen care of indirectly by checking balance
-      low = 10**100
-      host = nil
-      if !scache[:host_policy].empty?
-        scache[:host_policy].keys.peach do |hostkey|
-          load = Service.net_get("http://#{hostkey}:#{port}/load")
-          puts "load" + load.to_s + "low" + low.to_s
-          if load && (load.to_f < low) && self.policymatch(scache[:host_policy][hostkey], policyreq) 
-              low = load.to_f
-             
-              host = hostkey
-          end
+  # find machine that is up with absolute minimum load and appropriate policy
+  def self.minload(scache, policyreq, port)
+    # make sure it is up before assigning it as "low"
+    # taken care of indirectly by checking balance
+    low = 10**100
+    host = nil
+    if !scache[:host_policy].empty?
+      scache[:host_policy].keys.peach do |hostkey|
+        load = Service.net_get("http://#{hostkey}:#{port}/load")
+        puts "load" + load.to_s + "low" + low.to_s
+        if load && (load.to_f < low) && self.policymatch(scache[:host_policy][hostkey], policyreq)
+          low = load.to_f
+          host = hostkey
         end
       end
-      host = "http://#{host}:#{port}"   if host          
-       
+    end
+    host = "http://#{host}:#{port}" if host
   end
 
-  def self.policymatch(ptest, preq)     #parse boolean logic
-   
-     
-     
-     true  #for now, need to implement      
-    
-                 
+  # parse boolean logic
+  def self.policymatch(ptest, preq)
+    true  #for now, need to implement
   end
-  
+
   def self.getindex
     Rails.cache.read(Service.cache_key)
   end
-  
-  
+
   def self.getlist
-    
     s = []
-     if !Service::APPS.keys.empty?
-       Service::APPS.keys.peach do |namepolicy| 
-         s << [namepolicy, Service::APPS[namepolicy].last] if Service.up?(namepolicy)
-       end
-     end
-    
-     s
-     # render :json => s  model equivalent to this?
+    unless Service::APPS.keys.empty?
+      Service::APPS.keys.peach do |namepolicy|
+        s << [namepolicy, Service::APPS[namepolicy].last] if Service.up?(namepolicy)
+      end
+    end
+    s
   end
-  
+
   ##
   # returns a populated services object, generally for cacheing
   def self.discovery  #TODO: needs to be tested for newly designed hash
-   cache_me = {
+    cache_me = {
       :timestamp => nil,
       :machinepolicy => nil,
-   :services =>  Hash.new {|hash,key|
-                      hash[key] = {:host_policy => Hash.new{|hash,key|
-                                    hash[key] = nil},
-                                   :threshold => nil}
-                     }
+      :services => {
+          :host_policy => Hash.new{|hash,key| hash[key] = nil},
+          :threshold => nil
+      }
+    }
 
-   }
- 
-   serviceload = Hash.new(0)
+    serviceload = {}
     # remote call to services/list
     if !nmap.empty?
       nmap.peach do |box|
@@ -90,27 +80,29 @@ class Service < ActiveRecord::Base
         if !JSON.parse(json_out).empty?
           JSON.parse(json_out).peach do |namepolicy|
             cache_me[:services][namepolicy.first][:host_policy][box] = namepolicy.last
-            serviceload[namepolicy.first] = serviceload[namepolicy.first] + (net_get("http://#{box}:#{Service::APPS[namepolicy.first].first}/load").to_f || 0)
+            serviceload[namepolicy.first] +=
+              (net_get("http://#{box}:#{Service::APPS[namepolicy.first].first}/load").to_f || 0)
           end
         end
-       end
+      end
     end
-  
-   if !cache_me[:services].keys.empty?
-      cache_me[:services].keys.peach do |sname| 
-         cache_me[:services][sname][:threshold]  =  serviceload[sname]/cache_me[:services][sname][:host_policy].keys.size
-       end
+
+    if !cache_me[:services].keys.empty?
+      cache_me[:services].keys.peach do |sname|
+        cache_me[:services][sname][:threshold] =
+          serviceload[sname]/cache_me[:services][sname][:host_policy].keys.size
+      end
     end
+
     cache_me[:timestamp] = DateTime.now
     cache_me
-
   end
 
   def self.up?(name,host='localhost')
     if !net_get("http://#{host}:#{Service::APPS[name].first}")
-     return false
-   else
-    return true
+      return false
+    else
+      return true
     end
   end
 
@@ -127,11 +119,12 @@ class Service < ActiveRecord::Base
   def sync
     caches = []
     return if nmap.empty?
-      nmap.peach do |box| 
-        resp = net_get("http://#{box}:3000")
-        caches << JSON.parse(resp) if resp
-      end
-   
+
+    nmap.peach do |box|
+      resp = net_get("http://#{box}:3000")
+      caches << JSON.parse(resp) if resp
+    end
+
     newest = caches.sort{|a,b| a[:timestamp] <=> b[:timestamp]}.last
 
     # do a post to services/set_cache
@@ -139,7 +132,7 @@ class Service < ActiveRecord::Base
     post.set_form_data 'newest' => newest
 
     # perform the POST, the URI is always required
-    nmap.peach do |box| 
+    nmap.peach do |box|
       post_uri = URI "http://#{box}:3000/set_cache"
       Net::HTTP::Persistent.new.request post_uri, post
     end
@@ -152,15 +145,14 @@ class Service < ActiveRecord::Base
   # TODO: would need to give back obj eventually- return a tuple status (curb respose or false,  object (or nil in case of false))
   #problem: hangs on localhost:3000, only needed for services/list and get cache?
   def self.net_get(url)
-    if url.include?("#{`hostname`.strip}")  && url.include?(":3000") && url.include?("list")
+    if url.include?("#{`hostname`.strip}")  && url.include?(":3000")
+      if url.include?("list")
         return self.getlist.to_json
+      else
+        return self.index.to_json
+      end
     end
-    
-    if url.include?("#{`hostname`.strip}") && url.include?(":3000")
-         return self.index.to_json
-    end
-    
-    
+
     begin
       res = Net::HTTP::Persistent.new.request URI url
       res.body
