8634b8a7e0f432d9c327016f9f9a9ba84f0fc8e2
steve <steve@master.(none)>
2011-11-22 22:24:49 -0500

    change in measuring load

--- pi/app/controllers/load_controller.rb
+++ pi/app/controllers/load_controller.rb
@@ -2,10 +2,10 @@ class LoadController < ApplicationController
   def index
     # # of sessions and CPU usage
     connections = `netstat -an | grep :3141 |wc -l`.gsub!("\n","").to_i
-    memory = `pmap #{Process.pid} | tail -1`[10,40].strip.gsub!("K","").to_i
+    memory = (`pmap #{Process.pid} | tail -1`[10,40].strip.gsub!("K","").to_f*100.0) / (1024* `free -mt`.match(/Mem:\s*([0-9]+)/)[1].to_f)
     puts "Memory" + memory.to_s
     puts "connections" + connections.to_s
-    load =memory + memory*(connections*(1.0/100))
+    load =memory + memory*(connections*(5.0/100))
     render :text  => load.inspect
   end
 end
