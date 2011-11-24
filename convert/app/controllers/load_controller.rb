cd1b5596c1778ca4d0a8793d7da04ec308b10977
steve <steve@master.(none)>
2011-11-23 20:21:16 -0500

    I think i forgot to add metrics.rb in last commit,  it's now updated for proper http_posts  this commit also includes code cleanup in load controllers

--- convert/app/controllers/load_controller.rb
+++ convert/app/controllers/load_controller.rb
@@ -14,16 +14,12 @@ class LoadController < ApplicationController
     pid_array =  `ps -ef | grep java`.split(/\n/)
     pid_array.pop
     pid_array.pop
-    puts pid_array.last.to_s
     pid_array.each do |jproc|
          pid = jproc.match(/.+?([0-9]+)/)[1].to_i    #pid
          sysmem += (`pmap #{pid} | tail -1`[10,40].strip.gsub!("K","").to_f*100.0) / (1024* `free -mt`.match(/Mem:\s*([0-9]+)/)[1].to_f)
     end
     
      memory = (procmemory + sysmem)/2
-     
-     puts "Memory" + memory.to_s
-     puts "connections" + connections.to_s
      load =memory + memory*(connections*(5.0/100))
        # each connection adds 5% to load calculation, representing potential load, also to facilitate demonstration
      render :text  => load.inspect
